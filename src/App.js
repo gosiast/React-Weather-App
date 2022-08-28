f71f861a877ca25745e233099967bf0ca77d5bbe
Malgorzata Stano <stano.malgorzata@gmail.com>
2022-08-28 10:08:27 +0200

    imported boostrap

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
 import WeatherSearch from "./WeatherSearch";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
 	return (
 		<div className="App">
-			<div className= "weather-container">
+			<div className="weather-container">
 				<header className="App-header">
 					<h1> My Weather App - React</h1>
-				<WeatherSearch />
-			</header>
+					<WeatherSearch />
+				</header>
+			</div>
 			<footer>
 				{" "}
 				<a href="https://github.com/gosiast/react-weather-app">
@@ -16,7 +18,6 @@ function App() {
 				</a>{" "}
 				by <a href="https://github.com/gosiast">Malgorzata Stano</a>
 			</footer>
-			</div>
 		</div>
 	);
 }
